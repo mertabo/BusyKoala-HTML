c80af37648a32363d12796578c3eec095392674c
mrtabo1 <mrtabo1@up.edu.ph>
2022-07-07 13:58:38 +0800

    feat: create getAll to update DOM and show all attendance dated during the selected month in the selected workspace

--- assets/scripts/workspaces.js
+++ assets/scripts/workspaces.js
@@ -1,5 +1,5 @@
-function test() {
-  console.log("test");
+function endSession() {
+  console.log("Session has ended. Attendees can no longer time in.");
 }
 
 function getOwnWorkspace() {
@@ -28,7 +28,7 @@ function getOwnWorkspace() {
 
   const allLink = document.createElement("a");
   allLink.className = "nav-link";
-  allLink.onclick = test;
+  allLink.onclick = getAll;
   allLink.innerText = "All";
 
   allItem.appendChild(allLink);
@@ -52,14 +52,14 @@ function getToday() {
   container.innerHTML = "";
 
   // Fill the container with data
-  const dummyToday = [
+  const dummyData = [
     {name: "Millie Bobby Brown", time: ["9:12 AM"], duration: "0mins"},
     {name: "Taylor Swift", time: ["9:12 AM"], duration: "0mins"},
     {name: "Gaten Matarazzo", time: ["9:11 AM - 9:54 AM"], duration: "0mins"},
     {name: "Joe Keery", time: ["9:10 AM", "9:00 AM - 9:05 AM"], duration: "0mins"},
   ];
 
-  dummyToday.forEach (data => {
+  dummyData.forEach (data => {
     // Create card
     const card = document.createElement("div");
     card.className = "card";
@@ -118,6 +118,175 @@ function getToday() {
   });
 }
 
-function endSession() {
-  console.log("Session has ended. Attendees can no longer time in.");
+function toTitleCase(str) {
+  return str.split(' ')
+   .map(w => w[0].toUpperCase() + w.substring(1).toLowerCase())
+   .join(' ');
+}
+
+function getAll() {
+  const container = document.getElementById("midContentWSContainer");
+  
+  // Clear container by removing all cards
+  container.innerHTML = "";
+
+  // Get selected month
+  const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  const options = document.getElementsByClassName("active-card");
+  const month = months.indexOf(toTitleCase(options[1].innerText));
+
+  // Get selected workspace
+  const workspace = options[0].innerText.split("\n")[0];
+
+  // Dummy data
+  const dummyData = [
+    {
+      name: "Exist Software Labs, Inc.",
+      data: [
+        {
+          month: 6,
+          date: 3,
+          time: [
+            {name: "Millie Bobby Brown", time: ["9:05 AM - 10:30 AM"], duration: "1hr 25mins"},
+            {name: "Taylor Swift", time: ["9:04 AM - 10:04 AM"], duration: "1hr"},
+            {name: "Joe Keery", time: ["10:00 AM - 10:30 AM", "9:02 AM - 9:48 AM"], duration: "1hr 16mins"},
+            {name: "Gaten Matarazzo", time: ["8:59 AM - 10:30 AM"], duration: "1hr 31mins"},
+          ]
+        },
+        {
+          month: 6,
+          date: 2,
+          time: [
+            {name: "Millie Bobby Brown", time: ["9:05 AM - 10:30 AM"], duration: "1hr 25mins"},
+            {name: "Taylor Swift", time: ["9:04 AM - 10:04 AM"], duration: "1hr"},
+            {name: "Joe Keery", time: ["9:02 AM - 9:48 AM"], duration: "46mins"},
+            {name: "Gaten Matarazzo", time: ["8:59 AM - 10:30 AM"], duration: "1hr 31mins"},
+          ]
+        },
+        {
+          month: 5,
+          date: 3,
+          time: [
+            {name: "Millie Bobby Brown", time: ["9:05 AM - 10:30 AM"], duration: "1hr 25mins"},
+            {name: "Taylor Swift", time: ["9:04 AM - 10:04 AM"], duration: "1hr"},
+            {name: "Joe Keery", time: ["9:02 AM - 9:48 AM"], duration: "46mins"},
+            {name: "Gaten Matarazzo", time: ["8:59 AM - 10:30 AM"], duration: "1hr 31mins"},
+          ]
+        },
+      ]
+    },
+    {
+      name: "My Session",
+      data: [
+        {
+          month: 6,
+          date: 3,
+          time: [
+            {name: "Millie Bobby Brown", time: ["9:05 AM - 10:30 AM"], duration: "1hr 25mins"},
+            {name: "Taylor Swift", time: ["9:04 AM - 10:04 AM"], duration: "1hr"},
+            {name: "Joe Keery", time: ["9:02 AM - 9:48 AM"], duration: "46mins"},
+            {name: "Gaten Matarazzo", time: ["8:59 AM - 10:30 AM"], duration: "1hr 31mins"},
+          ]
+        },
+        
+      ]
+    },
+  ];
+
+  // Get data from dummy data
+  // Filter by workspace
+  let workspaceData = dummyData.filter(data => {
+    return data.name === workspace;
+  })[0].data;
+
+  // Filter by month
+  workspaceData = workspaceData.filter(data => {
+    return data.month === month;
+  });
+
+  // Fill container with data
+  workspaceData.forEach(monthData => {
+    // Create card
+    const card = document.createElement("div");
+    card.className = "card";
+    
+    const cardBody = document.createElement("div");
+    cardBody.className = "card-body";
+    
+    card.appendChild(cardBody);
+
+    // Add name to card
+    const name = document.createElement("h6");
+    name.className = "card-title";
+    name.innerText = `${months[monthData.month]} ${monthData.date}`;
+
+    cardBody.appendChild(name);
+
+    // Write number of attendees
+    const attendeesN = document.createElement("p");
+    attendeesN.className = "text-secondary";
+    attendeesN.innerText = `Number of attendees: ${monthData.time.length}`;
+
+    cardBody.appendChild(attendeesN);
+
+    // Create table
+    const div = document.createElement("div");
+    div.className = "text-secondary";
+
+    cardBody.appendChild(div);
+
+    // Create row
+    const row = document.createElement("div");
+    row.className = "row mt-2 fw-bold";
+
+    div.appendChild(row);
+
+    // Create column for name, time in and time out, and duration
+    const nameHeader = document.createElement("p");
+    nameHeader.className = "col-12 col-lg-4";
+    nameHeader.innerText = "Name";
+
+    const timeHeader = document.createElement("p");
+    timeHeader.className = "col-12 col-lg-4";
+    timeHeader.innerText = "Time in - Time out";
+
+    const durationHeader = document.createElement("p");
+    durationHeader.className = "col-12 col-lg-4";
+    durationHeader.innerText = "Duration";
+
+    row.append(nameHeader, timeHeader, durationHeader);
+
+    // Fill columns with data
+    monthData.time.forEach (timeData => {
+      // Create row
+      const row = document.createElement("div");
+      row.className = "row py-2 border-top";
+  
+      div.appendChild(row);
+
+      // Write name
+      const name = document.createElement("p");
+      name.className = "col-12 col-lg-4";
+      name.innerText = timeData.name;
+
+      // Write time
+      const time = document.createElement("div");
+      time.className = "col-12 col-lg-4";
+
+      timeData.time.forEach (timeN => {
+        const timeP = document.createElement("p");
+        timeP.innerText = timeN;
+        time.appendChild(timeP);
+      })
+
+      // Write duration
+      const duration = document.createElement("p");
+      duration.className = "col-12 col-lg-4";
+      duration.innerText = timeData.duration;
+
+      row.append(name, time, duration);
+    })
+
+    container.appendChild(card);
+  });
 }
